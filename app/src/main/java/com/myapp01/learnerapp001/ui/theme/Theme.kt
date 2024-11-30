7f296c69a90afb48091eebd462b382e183cff206
BlindMetropolis35 <pawann12095@gmail.com>
2024-11-30 15:21:33 +0530

    final commit

--- app/src/main/java/com/myapp01/learnerapp001/ui/theme/Theme.kt
+++ app/src/main/java/com/myapp01/learnerapp001/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.myapp01.learnerapp001.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
